36f7a9a2752585a1d61f00fc964fc351c1e4af61
qutaibaalani <qutaibaalani1984@gmail.com>
2023-07-17 15:43:59 -0400

    Added serializers for all models with all fields

--- WasteNotWizards/views.py
+++ WasteNotWizards/views.py
@@ -1,47 +1,57 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework import generics
 from .models import User, Provider, Receiver, Post, Reservation
-from .serializers import ProviderListSerializer
+from .serializers import (
+    ProviderListSerializer,
+    ReceiverListSerializer,
+    ProviderProfileSerializer,
+    ReceiverProfileSerializer,
+    PostListSerializer,
+    ReservationListSerializer,
+)
 
 # Create your views here.
 
 # Receiver List, Provider Profile, Receiver Profile, Post list, Receiver Reservation list
 
 
-# View for listing and creating Provider objects
+# For listing and creating instances of the `Provider` model.
 class ProviderListCreateView(generics.ListCreateAPIView):
     queryset = Provider.objects.all()
     serializer_class = ProviderListSerializer
 
 
-# View for listing Receiver objects
+# For listing instances of the `Receiver` model.
 class ReceiverListView(generics.ListAPIView):
     queryset = Receiver.objects.all()
-    serializer_class = ReceiverSerializer
+    serializer_class = ReceiverListSerializer
 
 
-# View for retrieving and updating Provider profile
+# For retrieving and updating the profile of a `Provider` instance.
 class ProviderProfileView(generics.RetrieveUpdateAPIView):
     queryset = Provider.objects.all()
-    serializer_class = ProviderSerializer
+    serializer_class = ProviderProfileSerializer
+    # The `lookup_field` is set to "user__username" to retrieve the profile based on the username of the associated user.
     lookup_field = "user__username"
 
 
-# View for retrieving and updating Receiver profile
+# For retrieving and updating the profile of a `Receiver` instance.
 class ReceiverProfileView(generics.RetrieveUpdateAPIView):
     queryset = Receiver.objects.all()
-    serializer_class = ReceiverSerializer
+    serializer_class = ReceiverProfileSerializer
+    # The `lookup_field` is set to "user__username" to retrieve the profile based on the username of the associated user.
     lookup_field = "user__username"
 
 
-# View for listing Post objects
+# For listing instances of the `Post` model.
 class PostListView(generics.ListAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostListSerializer
 
 
-# View for listing Receiver reservations
+# For listing reservations related to a `Receiver` instance.
 class ReceiverReservationListView(generics.ListAPIView):
     queryset = Reservation.objects.all()
-    serializer_class = ReservationSerializer
+    serializer_class = ReservationListSerializer
+    # The `lookup_field` is set to "receiver__user__username" to retrieve reservations based on the username of the associated receiver's user.
     lookup_field = "receiver__user__username"
