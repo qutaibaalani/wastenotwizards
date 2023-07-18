fb6119c78dafbd86de0c797cfddf979e39ea0cf0
superusermeaganrubino <meaganrubino@gmail.com>
2023-07-18 19:44:51 -0400

    componentscreated

--- Angularapp/src/app/app.module.ts
+++ Angularapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipientRegistrationComponent } from './recipient-registration/recipient-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipientRegistrationComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angularapp/src/app/recipient-registration/recipient-registration.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecipientRegistrationComponent } from './recipient-registration.component';
+
+describe('RecipientRegistrationComponent', () => {
+  let component: RecipientRegistrationComponent;
+  let fixture: ComponentFixture<RecipientRegistrationComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RecipientRegistrationComponent]
+    });
+    fixture = TestBed.createComponent(RecipientRegistrationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
