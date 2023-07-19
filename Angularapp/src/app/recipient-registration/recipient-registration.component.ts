06a6ca5dbd6fa497dacecc8d41a5307fbda24763
superusermeaganrubino <meaganrubino@gmail.com>
2023-07-18 21:12:57 -0400

    parts of reg component showing up html

--- Angularapp/src/app/recipient-registration/recipient-registration.component.ts
+++ Angularapp/src/app/recipient-registration/recipient-registration.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Recipient } from '../models/recipient.model';
 
 @Component({
-  selector: 'app-recipient-registration',
-  templateUrl: './recipient-registration.component.html',
-  styleUrls: ['./recipient-registration.component.css']
+    selector: 'app-recipient-registration',
+    templateUrl: './recipient-registration.component.html',
+    styleUrls: ['./recipient-registration.component.css']
 })
-export class RecipientRegistrationComponent {
+export class RecipientRegistrationComponent implements OnInit {
+
+    recipientForm: FormGroup = this.formBuilder.group({}); // initialize empty form
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit(): void {
+        this.recipientForm = this.formBuilder.group({
+            'username': ['', Validators.required],
+            'password': ['', [Validators.required, Validators.minLength(6)]],
+        });
+    }
+
+    onSubmit(): void {
+        if (this.recipientForm.valid) {
+            const newRecipient = new Recipient(
+                this.recipientForm.get('username')?.value,  /*chaining*/
+                this.recipientForm.get('password')?.value   
+            );
+            console.log(newRecipient);
+            
+        }
+    }
 
 }
