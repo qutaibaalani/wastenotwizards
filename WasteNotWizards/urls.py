6b61e97c547aa6435c7e00a5a2a6ab547f39c2df
qutaibaalani <qutaibaalani1984@gmail.com>
2023-07-19 15:29:01 -0400

    Added Post model and PostListView with serializer and url

--- WasteNotWizards/urls.py
+++ WasteNotWizards/urls.py
@@ -3,6 +3,6 @@ from WasteNotWizards import views
 
 
 urlpatterns = [
-    path('api/profile/<username>/', views.ProfileViewSet.as_view(), name='user_detail')
-
+    path("api/profile/<username>/", views.ProfileViewSet.as_view(), name="user_detail"),
+    path("api/posts/", views.PostListView.as_view(), name="post_list"),
 ]
