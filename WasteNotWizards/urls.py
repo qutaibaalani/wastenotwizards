a5b60a96076d9e390a618d2971bdb7506f710e3c
qutaibaalani <qutaibaalani1984@gmail.com>
2023-07-20 15:44:17 -0400

    Added receiver reservation

--- WasteNotWizards/urls.py
+++ WasteNotWizards/urls.py
@@ -5,4 +5,9 @@ from WasteNotWizards import views
 urlpatterns = [
     path("api/profile/<username>/", views.ProfileViewSet.as_view(), name="user_detail"),
     path("api/posts/", views.PostListView.as_view(), name="post_list"),
+    path(
+        "api/reservations/receiver/<str:username>/",
+        views.ReceiverReservationListView.as_view(),
+        name="receiver_reservation_list",
+    ),
 ]
