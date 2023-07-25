57f4029593c23189c0c5cb9817371533a35b6bce
Exo30 <mstucke2@ncsu.edu>
2023-07-25 15:37:15 -0400

    changed settings to fix authentication error

--- config/settings.py
+++ config/settings.py
@@ -172,4 +172,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'WasteNotWizards.User'
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
