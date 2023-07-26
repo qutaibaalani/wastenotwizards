08f2965ab1a9ec87a50da3f9bebf5fca0040813a
Exo30 <mstucke2@ncsu.edu>
2023-07-26 12:16:55 -0400

    angular.json hotfix for render

--- Angularapp/src/app/reservation/reservation.component.ts
+++ Angularapp/src/app/reservation/reservation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
