bba5017174f2ffbb29d78c0dd4a45f3009730800
Exo30 <mstucke2@ncsu.edu>
2023-07-28 21:21:12 -0400

    manually merging mapbox.components

--- Angularapp/src/app/mapbox/mapbox.component.ts
+++ Angularapp/src/app/mapbox/mapbox.component.ts
@@ -2,13 +2,17 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { PostAddressService } from './user-address.service';
+import { HttpClient } from '@angular/common/http';
+//import { ClosestPostsComponent, Post } from './post-list.component'; // Adjust the import path based on your project structure
 
 
-export interface PostAddress {
-  id: number;
-  address: string;
-  latitude: number;
-  longitude: number;
+export interface Address {
+  user_longitude: number;
+  user_latitude: number;
+}
+
+export interface post_address {
+  pins: Array<any>
 }
 
 @Component({
@@ -19,44 +23,78 @@ export interface PostAddress {
 export class MapBoxComponent implements OnInit {
   private map: mapboxgl.Map;
   private mapContainer: HTMLElement;
-  private addresses: PostAddress[]; // Replace 'any' with your specific address data type
+
+  private user_address: Address[]; 
+  private coordinates: any;
 
   constructor(private addressService: PostAddressService) {}
 
+
+  getUsernameFromLocalStorage(): void {
+    const storedData = localStorage.getItem('username');
+    if (storedData) {
+      const data = JSON.parse(storedData);
+      return data
+    }
+  }
+
+  getTokenFromLocalStorage(): void {
+    const storedData = localStorage.getItem('token')
+    if (storedData){
+      const data = JSON.parse(storedData)
+      return data
+    }else {
+      return undefined
+    }
+  }
+
+
   ngOnInit() {
-    setTimeout(() => {
+    const user = this.getUsernameFromLocalStorage()
+    const token = this.getTokenFromLocalStorage()
+
+    this.addressService.getUserAddresses(user, token).subscribe(
+      (user_info) => {
+        this.user_address = [user_info['user_longitude'], user_info['user_latitude']]
+      }
+    )
+
     this.addressService.getPostAddresses().subscribe(
-      (data) => {
-        this.addresses = data;
-        this.mapContainer = document.getElementById('map')
-        setTimeout(() => {
-          this.initMap(this.user_address, this.coordinates)
-        })
+      (post_info) => {
+        this.coordinates = []
+        post_info.forEach((pin)=> {
+          this.coordinates.push([pin.longitude, pin.latitude])
+        });
       },
       (error) => {
         console.error('Error fetching addresses:', error);
       }
     );
-  }, 2000);
+    this.mapContainer = document.getElementById('map')
+
+    setTimeout(() => {
+      this.initMap(this.user_address, this.coordinates)
+    }, 2000)
   }
 
-  private initMap() {
+  private initMap(user_coordinates, pin_coordinates) {
+    console.log(user_coordinates);
     (mapboxgl as any).accessToken = 'pk.eyJ1IjoibWVhZ2FucnViaW5vIiwiYSI6ImNsa2QweHh0czBzbzMzanBoamxlNWYwN3EifQ.Z1_FaWyOr3_mK9ErWinJFw';
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v12',
-      center: [43.02157, -74.10025], // Set the initial center of the map
+      center: new mapboxgl.LngLat(user_coordinates[0], user_coordinates[1]), // Set the initial center of the map
       zoom: 10 // Set the initial zoom level of the map
     });
-  
 
-
-    // Add map markers based on addresses
-    this.addresses.forEach((address) => {
-      const { longitude, latitude } = address;
+    if (pin_coordinates && Array.isArray(pin_coordinates)){
+    pin_coordinates.forEach((pin_arr) => {
+      var long = pin_arr[0]
+      var lat = pin_arr[1]
       new mapboxgl.Marker()
-        .setLngLat([longitude, latitude])
+        .setLngLat([long, lat])
         .addTo(this.map);
     });
+  } else {console.log("user_coordinates")}
   }
 }
