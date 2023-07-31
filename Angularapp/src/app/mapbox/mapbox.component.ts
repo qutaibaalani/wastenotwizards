601bb3a6fae03d87e7faf3bf7967f1a4ccfe1488
Exo30 <mstucke2@ncsu.edu>
2023-07-31 15:03:54 -0400

    working button

--- Angularapp/src/app/mapbox/mapbox.component.ts
+++ Angularapp/src/app/mapbox/mapbox.component.ts
@@ -34,6 +34,7 @@ export class MapBoxComponent implements OnInit {
   private map: mapboxgl.Map;
   private mapContainer: HTMLElement;
 
+  private post_id: any;
   private user_address: Address[]; 
   private coordinates: any;
 
@@ -135,13 +136,26 @@ export class MapBoxComponent implements OnInit {
   }
 
   public reserve(event, id) {
-    console.log('hello')
+    let token = this.getTokenFromLocalStorage()
     console.log(this.thisUser)
+    let url = 'https://waste-not-wizards.onrender.com/api/posts/' + id + '/'
+    console.log(url)
+    let data = {"reservation_status": "Closed", "reserved_by": this.thisUser}
 
     let currentDateTime = this.datepipe.transform((new Date), 'MM/dd/yyyy h:mm:ss');
-    console.log(currentDateTime)
-    this.http.patch('https://waste-not-wizards.onrender.com/api/posts/${id}/', {
-      "reservation_status": "Closed"
-    })
+    this.http.patch(url, data, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${token}`
+      }
+    }).subscribe(
+      res => {
+        console.log("working!!!!")
+    },
+    error => {
+      console.log("OOOHHH NOOO")
+    }
+    )
   }
 }
+
