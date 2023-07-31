b967181a2edca06116dc3bbaf1fe64a8978d1596
Exo30 <mstucke2@ncsu.edu>
2023-07-31 12:46:15 -0400

    logic for reservation

--- Angularapp/src/app/mapbox/mapbox.component.ts
+++ Angularapp/src/app/mapbox/mapbox.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ComponentFactoryResolver, ViewContainerRef } from '@
 import * as mapboxgl from 'mapbox-gl';
 import { PostAddressService } from './user-address.service';
 import { HttpClient } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
 import { PostListComponent } from '../post-list/post-list.component'; // Adjust the import path based on your project structure
 
 export interface Post {
@@ -20,6 +21,7 @@ export interface post_address {
 
 @Component({
   selector: 'app-map',
+  providers: [DatePipe],
   templateUrl: './mapbox.component.html',
   styleUrls: ['./mapbox.component.css']
 })
@@ -27,6 +29,7 @@ export class MapBoxComponent implements OnInit {
   loggers: any;
   closestPosts: Post[] = [];
   thisUser: any;
+
 
   private map: mapboxgl.Map;
   private mapContainer: HTMLElement;
@@ -37,7 +40,9 @@ export class MapBoxComponent implements OnInit {
   constructor(private addressService: PostAddressService, 
   private http: HttpClient,
   private componentFactoryResolver: ComponentFactoryResolver,
-  private viewContainerRef: ViewContainerRef  ) {}
+  private viewContainerRef: ViewContainerRef,
+  public datepipe: DatePipe) {let currentDateTime =this.datepipe.transform((new Date), 'MM/dd/yyyy h:mm:ss')}
+
 
 
   getUsernameFromLocalStorage() {
@@ -82,7 +87,7 @@ export class MapBoxComponent implements OnInit {
   }
 
   private fetchAndDisplayClosestPosts(user_coor): void {
-    this.http.get<Post[]>(`https://waste-not-wizards.onrender.com/api/closePosts?latitude=${user_coor[1]}&longitude=${user_coor[0]}`)
+    this.http.get<Post[]>(`https://waste-not-wizards.onrender.com/api/closePosts?latitude=${user_coor[1]}&longitude=${user_coor[0]}&reservation_status=Open`)
       .subscribe(
         (data) => {
           this.closestPosts = data.slice(0, 10);
@@ -98,7 +103,6 @@ export class MapBoxComponent implements OnInit {
 
 
   private initMap(user_coordinates, pin_coordinates) {
-    console.log(user_coordinates);
     (mapboxgl as any).accessToken = 'pk.eyJ1IjoibWVhZ2FucnViaW5vIiwiYSI6ImNsa2QweHh0czBzbzMzanBoamxlNWYwN3EifQ.Z1_FaWyOr3_mK9ErWinJFw';
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
@@ -118,8 +122,16 @@ export class MapBoxComponent implements OnInit {
   } else {console.log("user_coordinates")}
   }
 
-  private reserve(id) {
-    this.loggers = 'hello'
+  public reserve(id) {
+    console.log('hello')
     console.log(this.thisUser)
+
+    let currentDateTime = this.datepipe.transform((new Date), 'MM/dd/yyyy h:mm:ss');
+    console.log(currentDateTime)
+    this.http.patch('https://waste-not-wizards.onrender.com/api/posts/${id}/', {
+      "reservation_status": "Closed",
+      "reserved_by_id": id,
+      "reservation_time": currentDateTime,
+    })
   }
 }
